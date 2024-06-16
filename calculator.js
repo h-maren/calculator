ec0faefdd36906207c3f6f89f447016b5f665927
h-maren <26232423+h-maren@users.noreply.github.com>
2024-06-16 16:38:25 -0400

    Add operating function to button clicks on calculator Calculator working for one iteration - still need to fix if continuously putting in values

--- calculator.js
+++ calculator.js
@@ -25,12 +25,18 @@ function operate(firstNum,operator,secondNum){
             return add(firstNum,secondNum);
         case "-":
             return subtract(firstNum,secondNum);
-        case "*":
+        case "x":
             return multiply(firstNum,secondNum);
         case "/":
             return divide(firstNum,secondNum);
     };
 };
+
+
+let firstNumFlag=0; //this flag tells whether or not first number was already put in
+let secondNumFlag=0; //this flag tells whether or not second number was already put in
+let operatorFlag=0; //this flag tells whether operator has already been put in
+let inputValue=0;
 
 //when clicking on buttons, put in display
 const operatorButtons=document.querySelectorAll(".operator");
@@ -38,17 +44,51 @@ const operatorButtons=document.querySelectorAll(".operator");
 const numButtons=document.querySelectorAll(".num");
 //console.log(numButtons);
 let display=document.querySelector(".display");
+const equalsButton=document.querySelector(".equals");
 
 numButtons.forEach((button)=>{
     button.addEventListener("click", () => {
         let buttonText=button.textContent;
+        if((firstNumFlag==1)&&(operatorFlag==1)&&(secondNumFlag==0)){
+            //first clear the display 
+            display.textContent="";
+            //now we're doing secondNum
+            secondNumFlag=1;
+        }
         display.textContent+=buttonText;
-        let inputValue=display.textContent;
+        inputValue=display.textContent;
         console.log(inputValue);
         return inputValue;
-    })
+    });
 });
 
-let firstNumFlag=0; //this flag tells whether or not first number was already put in
-let secondNumFlag=0; //this flag tells whether or not second number was already put in
 
+operatorButtons.forEach((button)=>{
+    button.addEventListener("click", () => {
+        console.log(inputValue);
+        operator=button.textContent;
+        console.log(operator);
+        //operator has been selected so we set the flag
+        if(operatorFlag==0){//operator hasn't been selected yet
+            operatorFlag=1;
+        }
+        if((firstNumFlag==0)&&(secondNumFlag==0)){
+            firstNumFlag=1;
+            firstNum=inputValue;
+            console.log(firstNum);
+        };
+    });
+});
+
+
+//equals sign does the calculation
+equalsButton.addEventListener("click", () => {
+    if((firstNumFlag==1)&&(operatorFlag==1)&&(secondNumFlag=1)){
+        secondNum=inputValue;
+        console.log(secondNum);
+        console.log(`First num, operator, second num: ${firstNum}, ${operator},${secondNum}`);
+        let result=operate(firstNum,operator,secondNum);
+        console.log(result);
+        display.textContent=result;
+    }
+});
