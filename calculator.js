ba9fa7e8c47f90fb9487ad79b3ddc7e9d4b5ee71
h-maren <26232423+h-maren@users.noreply.github.com>
2024-06-17 12:45:00 -0400

    Add ability to do multiple operators Can now chain operators (i.e. 1+2+3 instead of 1+2=3+3), definitely still some bugs

--- calculator.js
+++ calculator.js
@@ -50,11 +50,13 @@ const clearButton=document.querySelector(".clear");
 numButtons.forEach((button)=>{
     button.addEventListener("click", () => {
         let buttonText=button.textContent;
-        if((firstNumFlag==1)&&(operatorFlag==1)&&(secondNumFlag==0)){
+        console.log(`First num, operator, second num flags: ${firstNumFlag},${operatorFlag},${secondNumFlag}`);
+        if((firstNumFlag==1)&&(operatorFlag==1)){
             //first clear the display 
             display.textContent="";
             //now we're doing secondNum
             secondNumFlag=1;
+            console.log("on second number now");
         }
         display.textContent+=buttonText;
         inputValue=display.textContent;
@@ -66,36 +68,56 @@ numButtons.forEach((button)=>{
 
 operatorButtons.forEach((button)=>{
     button.addEventListener("click", () => {
-        console.log(inputValue);
-        operator=button.textContent;
-        console.log(operator);
         //operator has been selected so we set the flag
+        console.log(inputValue);
         if(operatorFlag==0){//operator hasn't been selected yet
+            operator=button.textContent;
+            console.log(operator);
             operatorFlag=1;
+        }
+        else if((operatorFlag==1)&&((firstNumFlag==0)||(secondNumFlag==0))){
+            //break out of code if operator is set but not all numbers are and clear all values
+            alert("Double operators!");
+            clearAllValues();
         }
         if((firstNumFlag==0)&&(secondNumFlag==0)){
             firstNumFlag=1;
             firstNum=inputValue;
             console.log(firstNum);
         };
+        //clicking another operator after putting in second number should follow equals operator (relevant if statement is already in the equals sign)
+        console.log(`First num, operator, second num flags: ${firstNumFlag},${operatorFlag},${secondNumFlag}`);
+        equalFunction();
     });
 });
 
 
 //equals sign does the calculation
-equalsButton.addEventListener("click", () => {
-    if((firstNumFlag==1)&&(operatorFlag==1)&&(secondNumFlag=1)){
+function equalFunction () {
+    if((firstNumFlag==1)&&(operatorFlag==1)&&(secondNumFlag==1)){
         secondNum=inputValue;
         console.log(secondNum);
         console.log(`First num, operator, second num: ${firstNum}, ${operator},${secondNum}`);
         let result=operate(firstNum,operator,secondNum);
         console.log(result);
         display.textContent=result;
-    }
+        //after this, the result is now the firstNum (for additional operator), and clear the secondNum and operator
+        firstNum=result;
+        secondNum="";
+        secondNumFlag=0;
+    };
+};
+
+//function for clicking equals sign or for 
+equalsButton.addEventListener("click", () => {
+    equalFunction();
+    operatorFlag=0;
 });
 
 //when you press clear it clears all values
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", clearAllValues);
+
+function clearAllValues() {
     display.textContent="";
     firstNum="";
     secondNum="";
@@ -104,4 +126,5 @@ clearButton.addEventListener("click", () => {
     secondNumFlag=0;
     operatorFlag=0;
     inputValue=0;
-});
+};
+
